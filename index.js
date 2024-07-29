1be0b5054d0c7eeed019794840b2791a21b98036
Sandip kumar yadav <sandipyadav7541@gmail.com>
2024-07-29 12:05:40 +0530

    updating the api

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/payment', async (req, res) => {
                 "payment_method": "paypal"
             },
             "redirect_urls": {
-                "return_url": "https://pay-pal-server.vercel.app/",
+                "return_url": "https://pay-pal-server.vercel.app/success",
                 "cancel_url": "https://pay-pal-server.vercel.app/failed"
             },
             "transactions": [{
@@ -109,7 +109,7 @@ app.post('/paymentCoupon', async (req, res) => {
                 "payment_method": "paypal"
             },
             "redirect_urls": {
-                "return_url": "https://pay-pal-server.vercel.app/",
+                "return_url": "https://pay-pal-server.vercel.app/success",
                 "cancel_url": "https://pay-pal-server.vercel.app/failed"
             },
             "transactions": [{
@@ -153,7 +153,7 @@ app.post('/paymentCoupon', async (req, res) => {
 
 
 
-app.get('/', async (req, res) => {
+app.get('/success', async (req, res) => {
 
     try {
        const PayerID = req.query.PayerID;
